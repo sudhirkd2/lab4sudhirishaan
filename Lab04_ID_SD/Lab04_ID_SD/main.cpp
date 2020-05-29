bf92e2aa97ac474169f3471333491efe31152a5d
sdharmadhikari <iwilld0it>
2020-05-29 16:03:07 -0700

    Trial cpp

---
+++ Lab04_ID_SD/Lab04_ID_SD/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab4local
+//
+//  Created by Sudhir Dharmadhikari on 5/29/20.
+//  Copyright © 2020 Sudhir Dharmadhikari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, lab4!\n";
+    return 0;
+}
